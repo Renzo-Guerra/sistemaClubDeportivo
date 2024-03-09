cda6d92d479d15b5ee66937ac3baffcc31319029
renzo-guerra <renzoguerralatt@gmail.com>
2024-03-08 23:04:02 -0300

    Inicio proyecto

---
+++ src/main/java/com/canchas/sistema/SistemaClubDeportivoApplication.java
@@ -0,0 +1,13 @@
+package com.canchas.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaClubDeportivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaClubDeportivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canchas/sistema/SistemaClubDeportivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canchas.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaClubDeportivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
